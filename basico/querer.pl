63978bcd7edea324043a9f84227b8c6a5be60b5f
Damian Piazza <83801199+XkaunanX@users.noreply.github.com>
2024-10-27 23:31:54 -0300

    Add files via upload

---
+++ basico/querer.pl
@@ -0,0 +1,31 @@
+% Hechos
+eshombre(juan).
+eshombre(pedro).
+eshombre(luis).
+eshombre(carlos).
+eshombre(diego).
+eshombre(fernando).
+eshombre(roberto).
+eshombre(andres).
+eshombre(mateo).
+eshombre(alberto).
+quiere(juan, maria).
+quiere(juan, elena).
+quiere(pedro, raquel).
+quiere(pedro, belen).
+quiere(carlos, julia).
+quiere(diego, ana).
+quiere(fernando, gabriela).
+quiere(roberto, isabel).
+quiere(andres, carla).
+quiere(mateo, fatima).
+quiere(alberto, diana).
+
+% Consulta - Backtracking
+% eshombre(X),quiere(X,Y).
+% eshombre(X).
+% quiere(Hombre, _).
+% quiere(juan, Persona).
+% quiere(Hombre, Persona).
+% quiere(_, maria).
+% (quiere(Hombre, julia); quiere(Hombre, isabel)).
