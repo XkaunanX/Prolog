8517d5d16e4c01a47e6bf583414ba520bff2f872
Damian Piazza <83801199+XkaunanX@users.noreply.github.com>
2024-11-16 00:24:59 -0300

    Delete listas directory

--- listas/añadir.pl
+++
@@ -1,7 +0,0 @@
-add(X, [], [X]).  % Caso base: agregar a una lista vacía
-add(X, L, [X|L]).  % Agregar X al inicio de la lista L
-
-% Consultas
-% add(5, [], R). 
-% add(a, [b, c], R). 
-% add(10, [1, 2, 3], R).
